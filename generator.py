98183e00c4d4186c66a0e56ed9825ab064607b2f
asholok <asholok@gmail.com>
2014-05-21 14:52:44 +0300

    add date triger

--- generator.py
+++ generator.py
@@ -1,21 +1,24 @@
 import yaml
-# This utility creats tables to postgresql
+
+first_sentence 	= 'CREATE TABLE "{0}" ( \n	{0}_id serial primary key, \n'
+blanck 			= '	{}_{}  {}, \n'
+date_control 	= '	{0}_creation_date integer NOT NULL DEFAULT cast(extract(epoch from now()) as integer),\n	{0}_modified integer DEFAULT 0\n); \n\n'
+trigger 		= 'CREATE OR REPLACE FUNCTION update_{0}()\nRETURN TRIGGER AS $$\nBEGIN\n	UPDATE "{0}" SET {0}_modified = cast(extract(epoch from now()) as integer);\nEND;\n\nCREATE OR ALERT TRIGGER {0}_updated_trigger \n	AFTER UPDATE ON "{0}" \n	FOR EACH ROW EXECUTE PROCEDURE update_{0}();\n'
+
 def make_sql_request(table_name, fields):
-	id_field = '	{}_id serial primary key'.format(table_name)
-	sql = ['CREATE TABLE "{}" ( \n'.format(table_name)]
-
-	for key, value in fields.items():
-		sql.append('	{}_{}  {}, \n'.format(table_name, key, value))
+	sql = [first_sentence.format(table_name)]
 	
-	sql.append(id_field)
-	sql.append(' ); \n')
-
-	res = ''.join(sql)
-	return res
+	for key, value in fields.items():
+		sql.append(blanck.format(table_name, key, value))
+	
+	sql.append(date_control.format(table_name))
+	sql.append(trigger.format(table_name))
+	
+	return ''.join(sql)
 
 def make_string():	
 	sql_string = []
-	with open("tables.yaml", 'r') as f: # Plase and name of yaml file
+	with open("tables.yaml", 'r') as f:
 		schema = yaml.load(f.read())
 	for table, structure in schema.items():
 		sql_string.append(make_sql_request(table, structure["fields"]))
@@ -23,7 +26,7 @@ def make_string():
 	with open("psql.sql", 'w') as d:
 		d.write(statement)
 	
-	return statement # or return SQL statements in string container
+	return statement
 
 if __name__ == '__main__':
 	print(make_string())
--- psql.sql
+++ psql.sql
@@ -1,7 +1,33 @@
 CREATE TABLE "category" ( 
+	category_id serial primary key, 
 	category_title  VARCHAR(50), 
-	category_id serial primary key ); 
+	category_creation_date integer NOT NULL DEFAULT cast(extract(epoch from now()) as integer),
+	category_modified integer DEFAULT 0
+); 
+
+CREATE OR REPLACE FUNCTION update_category()
+RETURN TRIGGER AS $$
+BEGIN
+	UPDATE "category" SET category_modified = cast(extract(epoch from now()) as integer);
+END;
+
+CREATE OR ALERT TRIGGER category_updated_trigger 
+	AFTER UPDATE ON "category" 
+	FOR EACH ROW EXECUTE PROCEDURE update_category();
 CREATE TABLE "article" ( 
+	article_id serial primary key, 
 	article_text  TEXT, 
 	article_title  VARCHAR(50), 
-	article_id serial primary key ); 
+	article_creation_date integer NOT NULL DEFAULT cast(extract(epoch from now()) as integer),
+	article_modified integer DEFAULT 0
+); 
+
+CREATE OR REPLACE FUNCTION update_article()
+RETURN TRIGGER AS $$
+BEGIN
+	UPDATE "article" SET article_modified = cast(extract(epoch from now()) as integer);
+END;
+
+CREATE OR ALERT TRIGGER article_updated_trigger 
+	AFTER UPDATE ON "article" 
+	FOR EACH ROW EXECUTE PROCEDURE update_article();
